9ab0a1feeb871f827c41d3ec22dc63289558ac7d
Maggie-Sarmiento <gsmaggie001@gmail.com>
2022-01-26 09:01:25 -0600

    Version final

--- src/index.js
+++ src/index.js
@@ -5,16 +5,43 @@ import cipher from './cipher.js';
 function index(){
     // evento que escucha un click y llama a las funciones que guardan el mensaje y el desplazamiento
     document.getElementById('buttoncipher').addEventListener('click', function(){
-       let displacement = document.getElementById('displacement').value
+       let displacement = document.getElementById('inputdisplacement').value
        let msg = document.getElementById('inputmsg').value
-       document.getElementById('inputciphermsg').value = cipher.encode(displacement, msg)
+       document.getElementById('inputciphermsg').value = cipher.encode(displacement, msg, name)
     })
 
+    // evento que escucha un click y llama a las funciones que guardan el mensaje a descifrar y el desplazamiento
     document.getElementById('buttondecipher').addEventListener('click', function(){
-        let displacement = document.getElementById('displacement').value 
+        let displacement = document.getElementById('inputdisplacement').value 
         let msgtodecipher = document.getElementById('inputciphermsg').value
         document.getElementById('inputmsg').value = cipher.decode(displacement, msgtodecipher)
     })
+
+    // Seccion del codigo que muestra y oculta el pop de WhatSection
+    const buttonwhat = document.getElementById('popwhat')
+    const popupwhat = document.querySelector('.whatsectionpop')
+    buttonwhat.addEventListener('click', function(){
+        popupwhat.style.display = 'block'
+    })
+    document.getElementById('buttonBackWhat').addEventListener('click', function () {
+        popupwhat.style.display = 'none'
+    })
+
+    // Seccion del codigo que muestra y oculta el pop de WhatSection
+    const buttonhow = document.getElementById('pophow')
+    const popuphow = document.querySelector('.howsectionpop')
+    buttonhow.addEventListener('click', function(){
+        popuphow.style.display = 'block'
+    })
+    document.getElementById('buttonBackHow').addEventListener('click', function () {
+        popuphow.style.display = 'none'
+    })
+
+    // Boton que recarga la pagina
+    document.getElementById('buttonreset').onclick = function() {
+        location.reload(document.getElementById('main-section'))
+    }
+
 }
 
 
